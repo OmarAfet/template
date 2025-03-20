22bd845eca386a8b56f646d575139e127d63281c
OmarAfet <OmarAlenezy2004@gmail.com>
2025-03-20 06:18:43 +0300

    Refactor theme switching logic and clean up package.json

--- src/components/common/ThemeSwitcher.tsx
+++ src/components/common/ThemeSwitcher.tsx
@@ -12,12 +12,14 @@ export default function ThemeSwitcher() {
       setTheme('dark')
     } else if (theme === 'dark') {
       setTheme('light')
+    } else {
+      setTheme(theme === 'light' ? 'dark' : 'light')
     }
   }
 
   return (
     <Button onClick={toggleTheme} variant="ghost" size="icon" className="size-7">
-      <Sun  className="h-[1.2rem] w-[1.2rem] rotate-0  scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0  scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0   transition-all dark:rotate-0   dark:scale-100 absolute" />
       <span className="sr-only">Toggle theme</span>
     </Button>
